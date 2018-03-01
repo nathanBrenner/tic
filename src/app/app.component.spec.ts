32a374c2486547730ad884fbf2d7827c093330e6
nathan-j-brenner <nathan.j.brenner@gmail.com>
2018-03-01 15:30:56 -0800

    added test for app  component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
+import { BoardComponent } from './board/board.component';
+import { GameComponent } from './game/game.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-xdescribe('AppComponent', () => {
+import { SquareComponent } from './square/square.component';
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        GameComponent,
+        BoardComponent,
+        SquareComponent,
       ],
     }).compileComponents();
   }));
@@ -12,16 +18,5 @@ xdescribe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
